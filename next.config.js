c5846f704ed3fbcb350d78f5da3ed02a50c0520e
Tobías Nazareno Bregante <50115525+TobiasBregante@users.noreply.github.com>
2023-10-25 14:55:59 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,2 @@
 module.exports = {
-    env: {
-        API_PATH_PROD: 'https://apisicompralo.herokuapp.com',
-        APIT_PATH_DEV: 'http://localhost:4000',
-        API_PATH: 'https://apisicompralo.herokuapp.com'
-    }
 }
